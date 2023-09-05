7516e4d0b4fd00e7fd61564198fa0a8eabd51513
monten1234 <touch26.takehiro@icloud.com>
2023-09-05 23:50:00 +0900

    fuck

--- app.py
+++ app.py
@@ -1,29 +1,46 @@
-
+from dotenv import load_dotenv
+load_dotenv()
 import streamlit as st
 import os
-from supabase import create_client, Client
+from supabase import create_client
+from gotrue.errors import AuthApiError
 
-url: str = "https://clwmzkijmvdpxsgjwniz.supabase.co"
-key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsd216a2lqbXZkcHhzZ2p3bml6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMzMDU4MzIsImV4cCI6MjAwODg4MTgzMn0.2kpa137KbwfJTUQ2xQLb13SxYaHkCKgVkWaLc3soRDg"
+url = os.environ.get("SUPABASE_URL")
+key = os.environ.get("SUPABASE_KEY")
 # Supabaseクライアントを作成
-supabase: Client = create_client(url, key)
+supabase = create_client(url, key)
 
+#サインイン
+email: str = "touch26.takehiro@icloud.com"
+password: str = "Takehiro_1202"
+session = None
+try:
+    session = supabase.auth.sign_in_with_password({ "email": email, "password": password })
+except AuthApiError:
+    print("login failed")
+supabase.postgrest.auth(session.session.access_token) 
+# data = supabase.table("demo1").insert({"name": "item5"}).execute()
 # Streamlitアプリの設定
 st.title("Supabase Data Inserter")
 st.write("Upload a file to Supabase Table")
-id = st.text_input("ID")
+# id = st.text_input("ID")
 name = st.text_input("Name")
 insert_data = {
-    "id": id, 
     "name": name
-    }
+}
+
+id_value = 0
+
 if st.button("Insert Data"):
     if name:
         data = supabase.table("demo1").insert(insert_data).execute()
+        id_value = data.data[0]["id"]
     if data:
         st.success("Data uploaded successfully!")  
     else:
         st.error("Data upload failed.")  
-
+a = supabase.table("demo1").select("*").eq("id", id_value).execute()
+if a:
+    st.write(a)
 
 
